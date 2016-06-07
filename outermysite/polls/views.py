3e0044e1b2dd8fc2394fc014241cd121720bc384
Abhishek Upadhyay <abhishek.upadhyay.cse12@iitbhu.ac.in>
2016-06-07 16:14:44 +0530

    followed style guide stricty

--- outermysite/polls/views.py
+++ outermysite/polls/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-import json,requests,sys,traceback
+import json
+import requests
+import sys
+import traceback
 import pdb
 import imdbScraper
+
 
 # Create your views here.
 def searchMovie(request):
 
-    movie  = request.GET.get('q', '')
+    movie = request.GET.get('q', '')
     movie_list = imdbScraper.getSearchResults(movie)
 
-    paginator = Paginator(movie_list,20)
+    paginator = Paginator(movie_list, 20)
     page = request.GET.get('page')
 
     try:
@@ -22,15 +26,15 @@ def searchMovie(request):
     except EmptyPage:
         movies = paginator.page(1)
 
-    #print type(movies)
+    # print type(movies)
     movies = movies.object_list
     data = json.dumps(movies)
-    
+
     return HttpResponse(data, content_type='application/json')
-    
+
 
 def exactMovie(request):
-   
+
     Id = request.GET.get('q', '')
     data = imdbScraper.getMovieResults(Id)
     data = json.dumps(data)
